6ef67d5a5e33319c283071170cd4f1193ddceff6
malbarisumit <malbarisumit@gmail.com>
2021-01-31 21:11:46 -0500

    Department Highest Salary

---
+++ LeetCode_184_Department Highest Salary.sql
@@ -0,0 +1,56 @@
+Create table If Not Exists Employee (Id int, Name varchar(255), Salary int, DepartmentId int)
+Create table If Not Exists Department (Id int, Name varchar(255))
+Truncate table Employee
+insert into Employee (Id, Name, Salary, DepartmentId) values ('1', 'Joe', '70000', '1')
+insert into Employee (Id, Name, Salary, DepartmentId) values ('2', 'Jim', '90000', '1')
+insert into Employee (Id, Name, Salary, DepartmentId) values ('3', 'Henry', '80000', '2')
+insert into Employee (Id, Name, Salary, DepartmentId) values ('4', 'Sam', '60000', '2')
+insert into Employee (Id, Name, Salary, DepartmentId) values ('5', 'Max', '90000', '1')
+Truncate table Department
+insert into Department (Id, Name) values ('1', 'IT')
+insert into Department (Id, Name) values ('2', 'Sales')
+
+
+
+-- The Employee table holds all employees. Every employee has an Id, a salary, and there is also a column for the department Id.
+
+-- +----+-------+--------+--------------+
+-- | Id | Name  | Salary | DepartmentId |
+-- +----+-------+--------+--------------+
+-- | 1  | Joe   | 70000  | 1            |
+-- | 2  | Jim   | 90000  | 1            |
+-- | 3  | Henry | 80000  | 2            |
+-- | 4  | Sam   | 60000  | 2            |
+-- | 5  | Max   | 90000  | 1            |
+-- +----+-------+--------+--------------+
+-- The Department table holds all departments of the company.
+
+-- +----+----------+
+-- | Id | Name     |
+-- +----+----------+
+-- | 1  | IT       |
+-- | 2  | Sales    |
+-- +----+----------+
+-- Write a SQL query to find employees who have the highest salary in each of the departments. For the above tables, your SQL query should return the following rows (order of rows does not matter).
+
+-- +------------+----------+--------+
+-- | Department | Employee | Salary |
+-- +------------+----------+--------+
+-- | IT         | Max      | 90000  |
+-- | IT         | Jim      | 90000  |
+-- | Sales      | Henry    | 80000  |
+-- +------------+----------+--------+
+-- Explanation:
+
+-- Max and Jim both have the highest salary in the IT department and Henry has the highest salary 
+--in the Sales department.
+
+-- # Write your MySQL query statement below
+
+
+with temp as(
+SELECT d.Name as Department, e.Name as Employee, Salary, Rank() over (partition by  d.Name order by Salary desc)  as Ranki from Employee e inner join 
+Department d on DepartmentId = d.Id
+group by d.Name, e.Name
+    )
+select Department, Employee, Salary from temp where Ranki =1
